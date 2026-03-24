c5d42e17b4dca93873f6e2806827cee5a1b134a6
Imad Ahmed <j166a775433@gmail.com>
2026-03-24 14:09:32 +0000

    feat: add calculate mean method

---
+++ TDD-Communication.Tests/CalculateMeanTests.cs
@@ -0,0 +1,31 @@
+﻿using Shouldly;
+
+namespace TDD_Communication.Tests;
+
+public class CalculateMeanTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void CalculateTheMean_ShouldReturnZero_WhenArrayIsEmpty()
+    {
+        double[] input = [];
+
+        double output = CalculateMean.CalculateTheMean(input);
+
+        output.ShouldBe(0);
+    }
+    [Test]
+    public void CalculateTheMean_ShouldReturnTheMean()
+    {
+        double[] input = [40, 16, 5, 8];
+
+        double output = CalculateMean.CalculateTheMean(input);
+
+        output.ShouldBe(17.25);
+    }
+
+}
---
+++ TDD-Communication/CalculateMean.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Communication
+{
+    public class CalculateMean
+    {
+        public static double CalculateTheMean(double[] input)
+        {
+            if (input.Length == 0)
+            {
+                return 0;
+            }
+
+            double output = (double)(input.Sum() / input.Length);
+
+            return output;
+        }
+    }
+}
